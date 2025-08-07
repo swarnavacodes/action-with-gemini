f17f9e820be97b0082d0bbf29ca833c55bd486ce
swarnava <sg.learning001@gmail.com>
2025-08-06 21:26:34 -0500

    sample-project

--- example-projects/index.js
+++ example-projects/index.js
@@ -8,4 +8,10 @@ log(chalk.green(
     'I am a green line ' +
     chalk.blue.underline.bold('with a blue substring') +
     ' that becomes green again!'
-));
+))
+log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
+;
