6fae327efcad84f99ac0889ae42abce70ccecbe4
swarnava <sg.learning001@gmail.com>
2025-08-06 21:23:50 -0500

    new sample project

---
+++ example-projects/index.js
@@ -0,0 +1,11 @@
+// simple code to write colorful logs using chalk
+const chalk = require('chalk');
+const log = console.log;
+
+log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+log(chalk.green(
+    'I am a green line ' +
+    chalk.blue.underline.bold('with a blue substring') +
+    ' that becomes green again!'
+));
